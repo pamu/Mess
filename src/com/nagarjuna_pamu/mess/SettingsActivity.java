c55192d0f4e3f6cdbafe39a8dec1a6f29ff4a197
pamu <nagarjuna.pamu@gmail.com>
2014-05-10 15:24:15 -0400

    10th may 2014 3:24pm

--- src/com/nagarjuna_pamu/mess/SettingsActivity.java
+++ src/com/nagarjuna_pamu/mess/SettingsActivity.java
@@ -2,8 +2,6 @@ package com.nagarjuna_pamu.mess;
 
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class SettingsActivity extends PreferenceActivity {
 
