e4b9c03086ddc87bff4adec1755a963d4d6695fa
pamu <nagarjuna.pamu@gmail.com>
2014-05-10 15:18:12 -0400

    10th may 2014 3:18pm

--- src/com/nagarjuna_pamu/mess/SettingsActivity.java
+++ src/com/nagarjuna_pamu/mess/SettingsActivity.java
@@ -11,12 +11,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
 
-public class SettingsPreferencesActivity extends ActionBarActivity {
+public class SettingsActivity extends ActionBarActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_settings_preferences);
+		setContentView(R.layout.activity_settings);
 
 		if (savedInstanceState == null) {
 			getSupportFragmentManager().beginTransaction()
@@ -28,7 +28,7 @@ public class SettingsPreferencesActivity extends ActionBarActivity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.settings_preferences, menu);
+		getMenuInflater().inflate(R.menu.settings, menu);
 		return true;
 	}
 
@@ -55,8 +55,8 @@ public class SettingsPreferencesActivity extends ActionBarActivity {
 		@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container,
 				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(
-					R.layout.fragment_settings_preferences, container, false);
+			View rootView = inflater.inflate(R.layout.fragment_settings,
+					container, false);
 			return rootView;
 		}
 	}
